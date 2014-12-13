ffd7c01af234b43743e5b797621ab295ed9bfd49
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:47:50 +0100

    little shorter

--- ant/Program.fs
+++ ant/Program.fs
@@ -2,11 +2,11 @@
 
 [<EntryPoint>]
 let main argv = 
-    let step ((pos: int*int), (dir: byte), (blacks: Set<int*int>)) =
+    let s ((pos: int*int), dir , (b: Set<int*int>)) =
         let pos = match dir with | 0uy -> (fst pos + 1, snd pos) | 64uy -> (fst pos,snd pos + 1) | 128uy -> (fst pos - 1, snd pos) | _ -> (fst pos, snd pos - 1)
-        if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
+        if b.Contains(pos) then (pos, dir + 64uy, b.Remove(pos)) else (pos, dir - 64uy, b.Add(pos))
     let repeat n f = List.fold (>>) id (List.replicate n f)
-    let play a b c turns = repeat turns step (a, b, c)
+    let play a b c turns = repeat turns s (a, b, c)
     printfn "%A" (play (0,0) 0uy Set.empty 15001)
     System.Console.ReadLine() |> ignore
     0 
