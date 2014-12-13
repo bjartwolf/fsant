8299f5bd8a035e62a4fff9972997027c92f77da5
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:38:56 +0100

    even shorter

--- ant/Program.fs
+++ ant/Program.fs
@@ -5,8 +5,7 @@ type Blacks = Set<Pos>
 type Dir = byte 
 type State = Pos * Dir * Blacks 
 
-let step (state : State) =
-    let pos, dir, blacks = state
+let step ((pos: Pos), (dir: Dir), (blacks: Blacks)) =
     let x,y = pos
     let pos = match dir with | 0uy -> (x+1, y) | 64uy -> (x,y+1) | 128uy -> (x-1, y) | _ -> (x,y-1)
     if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
@@ -14,7 +13,7 @@ let step (state : State) =
 [<EntryPoint>]
 let main argv = 
     let repeat n f = List.fold (>>) id (List.replicate n f)
-    let play state turns = repeat turns step state
-    printfn "%A" (play ((0,0),0uy, Set.empty) 15001)
+    let play a b c turns = repeat turns step (a, b, c)
+    printfn "%A" (play (0,0) 0uy Set.empty 15001)
     System.Console.ReadLine() |> ignore
     0 
