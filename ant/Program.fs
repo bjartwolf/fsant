e50b8d1784fbe9680de0646cc7637c2981f48154
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:48:09 +0100

    and a little

--- ant/Program.fs
+++ ant/Program.fs
@@ -2,7 +2,7 @@
 
 [<EntryPoint>]
 let main argv = 
-    let s ((pos: int*int), dir , (b: Set<int*int>)) =
+    let s (pos, dir , (b: Set<int*int>)) =
         let pos = match dir with | 0uy -> (fst pos + 1, snd pos) | 64uy -> (fst pos,snd pos + 1) | 128uy -> (fst pos - 1, snd pos) | _ -> (fst pos, snd pos - 1)
         if b.Contains(pos) then (pos, dir + 64uy, b.Remove(pos)) else (pos, dir - 64uy, b.Add(pos))
     let repeat n f = List.fold (>>) id (List.replicate n f)
