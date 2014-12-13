75b88a934133ded2364aa6f7b867a8b7f8fe12de
bjartwolf <bjartnes@gmail.com>
2014-12-13 23:01:03 +0100

    shorter

--- ant/Program.fs
+++ ant/Program.fs
@@ -4,8 +4,8 @@
 let main argv = 
     let s (p, d, (b: Set<int*int>)) = let p = match d with | 0uy -> (fst p + 1, snd p) | 64uy -> (fst p,snd p + 1) | 128uy -> (fst p - 1, snd p) | _ -> (fst p, snd p - 1)
                                       if b.Contains(p) then (p, d + 64uy, b.Remove(p)) else (p, d - 64uy, b.Add(p))
-    let r n f = List.fold (>>) id (List.replicate n f)
-    let p t a b c = r t s (a, b, c)
-    printfn "%A" (p 15001 (0,0) 0uy Set.empty)
+    let r f = List.fold (>>) id (List.replicate 15001 f)
+    let p a b c = r s (a, b, c)
+    printfn "%A" (p (0,0) 0uy Set.empty)
     System.Console.ReadLine() |> ignore
     0 
