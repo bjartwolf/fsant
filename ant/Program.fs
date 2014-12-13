9fb12736fd668f55a7e656f1266efd12375b4bfb
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:46:42 +0100

    struggling to reach a tweet

--- ant/Program.fs
+++ ant/Program.fs
@@ -3,8 +3,7 @@
 [<EntryPoint>]
 let main argv = 
     let step ((pos: int*int), (dir: byte), (blacks: Set<int*int>)) =
-        let x,y = pos
-        let pos = match dir with | 0uy -> (x+1, y) | 64uy -> (x,y+1) | 128uy -> (x-1, y) | _ -> (x,y-1)
+        let pos = match dir with | 0uy -> (fst pos + 1, snd pos) | 64uy -> (fst pos,snd pos + 1) | 128uy -> (fst pos - 1, snd pos) | _ -> (fst pos, snd pos - 1)
         if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
     let repeat n f = List.fold (>>) id (List.replicate n f)
     let play a b c turns = repeat turns step (a, b, c)
