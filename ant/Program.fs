161440fa4e8b1b43a5aceb9088ffc3681504aabf
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:36:31 +0100

    shorter

--- ant/Program.fs
+++ ant/Program.fs
@@ -8,15 +8,8 @@ type State = Pos * Dir * Blacks
 let step (state : State) =
     let pos, dir, blacks = state
     let x,y = pos
-    let pos = match dir with 
-                | 0uy -> (x+1, y)
-                | 64uy -> (x,y+1) 
-                | 128uy -> (x-1, y) 
-                | _ -> (x,y-1)
-    if blacks.Contains(pos) then 
-        (pos, dir + 64uy, blacks.Remove(pos))
-    else  
-        (pos, dir - 64uy, blacks.Add(pos))
+    let pos = match dir with | 0uy -> (x+1, y) | 64uy -> (x,y+1) | 128uy -> (x-1, y) | _ -> (x,y-1)
+    if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
 
 [<EntryPoint>]
 let main argv = 
