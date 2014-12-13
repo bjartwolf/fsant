7610aea3f7ad157108c4d75dc58d85b82b5e18ab
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:11:49 +0100

    Applying C64 tricks

--- ant/Program.fs
+++ ant/Program.fs
@@ -2,30 +2,20 @@
 
 type Pos = int*int
 type Blacks = Set<Pos>
-type Dir = Right | Up | Left | Down
+type Dir = byte 
 type State = Pos * Dir * Blacks 
 
 let moveInDirection pos dir = 
     let x,y = pos
     match dir with 
-        | Right -> (x+1, y)
-        | Up -> (x,y+1) 
-        | Left -> (x-1, y) 
-        | Down -> (x,y-1)
+        | 0uy -> (x+1, y)
+        | 64uy -> (x,y+1) 
+        | 128uy -> (x-1, y) 
+        | _ -> (x,y-1)
 
-let turnLeft dir =
-    match dir with
-        | Right -> Up
-        | Up -> Left 
-        | Left -> Down
-        | Down -> Right 
+let turnLeft dir = dir + 64uy
 
-let turnRight dir =
-    match dir with
-        | Right -> Down 
-        | Up -> Right 
-        | Left -> Up 
-        | Down -> Left 
+let turnRight dir = dir - 64uy
 
 let step (state : State) =
     let pos, dir, blacks = state
@@ -40,6 +30,6 @@ let step (state : State) =
 let main argv = 
     let repeat n f = List.fold (>>) id (List.replicate n f)
     let play state turns = repeat turns step state
-    printfn "%A" (play ((0,0),Right, Set.empty) 15001)
+    printfn "%A" (play ((0,0),0uy, Set.empty) 15001)
     System.Console.ReadLine() |> ignore
     0 
