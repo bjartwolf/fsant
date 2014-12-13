7466d580bd288f26c806176054563d760efb820c
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:39:19 +0100

    compact stuff

--- ant/Program.fs
+++ ant/Program.fs
@@ -5,13 +5,13 @@ type Blacks = Set<Pos>
 type Dir = byte 
 type State = Pos * Dir * Blacks 
 
-let step ((pos: Pos), (dir: Dir), (blacks: Blacks)) =
-    let x,y = pos
-    let pos = match dir with | 0uy -> (x+1, y) | 64uy -> (x,y+1) | 128uy -> (x-1, y) | _ -> (x,y-1)
-    if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
 
 [<EntryPoint>]
 let main argv = 
+    let step ((pos: Pos), (dir: Dir), (blacks: Blacks)) =
+        let x,y = pos
+        let pos = match dir with | 0uy -> (x+1, y) | 64uy -> (x,y+1) | 128uy -> (x-1, y) | _ -> (x,y-1)
+        if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
     let repeat n f = List.fold (>>) id (List.replicate n f)
     let play a b c turns = repeat turns step (a, b, c)
     printfn "%A" (play (0,0) 0uy Set.empty 15001)
