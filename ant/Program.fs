aedfd7f9e3fa4cc10784ae06e20f9f811477145e
bjartwolf <bjartnes@gmail.com>
2014-12-13 22:39:59 +0100

    and shorter

--- ant/Program.fs
+++ ant/Program.fs
@@ -1,14 +1,8 @@
 ﻿module ant
 
-type Pos = int*int
-type Blacks = Set<Pos>
-type Dir = byte 
-type State = Pos * Dir * Blacks 
-
-
 [<EntryPoint>]
 let main argv = 
-    let step ((pos: Pos), (dir: Dir), (blacks: Blacks)) =
+    let step ((pos: int*int), (dir: byte), (blacks: Set<int*int>)) =
         let x,y = pos
         let pos = match dir with | 0uy -> (x+1, y) | 64uy -> (x,y+1) | 128uy -> (x-1, y) | _ -> (x,y-1)
         if blacks.Contains(pos) then (pos, dir + 64uy, blacks.Remove(pos)) else (pos, dir - 64uy, blacks.Add(pos))
