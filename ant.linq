d5c956d151a45536fc232bd5b69a402abe378cc9
bjartwolf <bjartnes@gmail.com>
2014-12-14 21:40:11 +0100

    vet ikke om jeg sprarte noe.

--- ant.linq
+++ ant.linq
@@ -3,8 +3,8 @@
 </Query>
 
 let _,_, points = List.fold(fun((x,y),d,b)_->
-	let m=List.nth[0;1;0;-1;0]in
-	let p=x+m(d+1),y+m d in
+	let a=[|0;1;0;-1;0|] in
+	let p=x+a.[d+1],y+a.[d] in
 	p,(d+if b-set[p]=b then 3 else 1)%4,(b-set[p])+(set[p]-b))((100,100),0,set[])[0..15000]
 let bitmap = new Bitmap(400,400)
 points |> Seq.iter (fun (x,y) -> bitmap.SetPixel(x,y,Color.Black))
--- ant/Program.fs
+++ ant/Program.fs
@@ -2,6 +2,6 @@
 
 [<EntryPoint>]
 let main argv = 
-    List.fold(fun((x,y),d,b)_->let m=List.nth[0;1;0;-1;0]in let p=x+m(d+1),y+m d in p,(d+if b-set[p]=b then 3 else 1)%4,(b-set[p])+(set[p]-b))((100,100),0,set[])[0..15000] |> printfn "%A" 
+    List.fold(fun((x,y),d,b)_-> let a=[|0;1;0;-1;0|] in let p=x+a.[d+1],y+a.[d] in p,(d+if b-set[p]=b then 3 else 1)%4,(b-set[p])+(set[p]-b))((100,100),0,set[])[0..15000] |> printfn "%A"
     System.Console.ReadLine() |> ignore
     0 
